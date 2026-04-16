4e02ac89e3dc66f4600c9e9b4c97c76be482241c
waliulnizu <waliul.nizu@gmail.com>
2026-04-17 03:50:46 +0600

    added loading animation

--- src/components/Friends.jsx
+++ src/components/Friends.jsx
@@ -1,15 +1,36 @@
+"use client";
+
 import FriendCard from "./FriendCard";
-import friends from "@/data/friends.json";
+import { useState, useEffect } from "react";
+import Loading from "./Loading";
+import friendsData from "@/data/friends.json";
 
 const Friends = () => {
+
+  const [friends, setFriends] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+
+    setTimeout(() => {
+      setFriends(friendsData);
+      setLoading(false);
+    }, 1000);
+
+  }, []);
+
+  if (loading) {
+    return <Loading />;
+  }
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 max-w-6xl mx-auto mt-8">
 
       {
         friends.map(friend => (
-          <FriendCard 
-          key={friend.id}
-          friend={friend}
+          <FriendCard
+            key={friend.id}
+            friend={friend}
           />
         ))
       }
---
+++ src/components/Loading.jsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+  return (
+    <div className="text-center py-10">
+      Loading...
+    </div>
+  );
+};
+
+export default Loading;
