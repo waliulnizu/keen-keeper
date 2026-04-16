4ca8dc1c6ad216590ee3512c66b6c88ff4150623
waliulnizu <waliul.nizu@gmail.com>
2026-04-17 02:36:08 +0600

    added dynamic friend details data

--- src/app/friend/[id]/page.jsx
+++ src/app/friend/[id]/page.jsx
@@ -1,10 +1,26 @@
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import friends from "@/data/friends.json";
+import Image from "next/image";
 
 const FriendDetails = async ({ params }) => {
 
     const { id } = await params;
-    // console.log(id)
+    const friend = friends.find(
+    f => f.id === parseInt(id)
+  );
+
+  if (!friend) {
+    return (
+      <div>
+        <Navbar />
+        <div className="max-w-6xl mx-auto py-10">
+          <h1 className="text-2xl font-bold">Friend not found</h1>
+        </div>
+        <Footer />
+      </div>
+    );
+  }
 
   return (
     <div>
@@ -13,10 +29,24 @@ const FriendDetails = async ({ params }) => {
       <div className="max-w-6xl mx-auto py-10">
 
         <h1 className="text-2xl font-bold">
-          Friend Details Page
+          {friend.name}
         </h1>
 
-        <p>ID: {id}</p>
+        <Image 
+        src={friend.picture}
+        alt={friend.name}
+        height={64}
+        width={64}
+        className="w-24 h-24 rounded-full mt-4"
+        />
+
+        <p className="mt-4">
+          {friend.bio}
+        </p>
+
+        <p className="mt-2">
+          {friend.email}
+        </p>
 
       </div>
 
