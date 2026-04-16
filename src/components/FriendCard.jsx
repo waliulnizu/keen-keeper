edafe71e850fbfc3fbeb6645e385bc8363ccdcfc
waliulnizu <waliul.nizu@gmail.com>
2026-04-17 01:48:53 +0600

    json file move to src/data folder add more data in json file and converted friend cards to dynamic

--- src/components/FriendCard.jsx
+++ src/components/FriendCard.jsx
@@ -1,29 +1,33 @@
 import Image from "next/image";
 
-const FriendCard = () => {
+const FriendCard = ({ friend }) => {
+  const { name, picture, days_since_contact, status, tags } = friend;
+
   return (
     <div className="shadow p-4 rounded text-center">
-
       <Image
-        src="https://www.startpage.com/av/proxy-image?piurl=https%3A%2F%2Fimg.freepik.com%2Ffree-photo%2Fyoung-bearded-man-with-striped-shirt_273609-5677.jpg%3Fsemt%3Dais_hybrid%26w%3D740%26q%3D80&sp=1776355853T727533966609280f41dd960dfb9ac4ba7b0195c9c4ea93ad27f9d39f082fad41"
+        src={picture}
         width={64}
         height={64}
+        alt={name}
         className="w-16 h-16 rounded-full mx-auto"
-        alt="friends-image"
       />
 
-      <h2 className="font-bold mt-2">
-        Emma Wilson
-      </h2>
+      <h2 className="font-bold mt-2">{name}</h2>
 
-      <p className="text-gray-500">
-        62 days ago
-      </p>
+      <p className="text-gray-500">{days_since_contact} days ago</p>
 
-      <span className="bg-red-100 text-red-500 px-2 py-1 rounded text-sm">
-        Overdue
+      <div className="flex gap-2 justify-center mt-2">
+        {tags.map((tag, index) => (
+          <span key={index} className="bg-gray-200 px-2 py-1 text-sm rounded">
+            {tag}
+          </span>
+        ))}
+      </div>
+
+      <span className="bg-red-100 text-red-500 px-2 py-1 rounded text-sm mt-2 inline-block">
+        {status}
       </span>
-
     </div>
   );
 };
--- src/components/Friends.jsx
+++ src/components/Friends.jsx
@@ -1,17 +1,18 @@
 import FriendCard from "./FriendCard";
+import friends from "@/data/friends.json";
 
 const Friends = () => {
   return (
     <div className="grid grid-cols-4 gap-4 max-w-6xl mx-auto mt-8">
 
-      <FriendCard />
-      <FriendCard />
-      <FriendCard />
-      <FriendCard />
-      <FriendCard />
-      <FriendCard />
-      <FriendCard />
-      <FriendCard />
+      {
+        friends.map(friend => (
+          <FriendCard 
+          key={friend.id}
+          friend={friend}
+          />
+        ))
+      }
 
     </div>
   );
