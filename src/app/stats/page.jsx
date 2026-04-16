95fa01ab17bc10d157d95e88cdf6e90d8f94fb1f
waliulnizu <waliul.nizu@gmail.com>
2026-04-16 22:44:25 +0600

    added stats page static

---
+++ src/app/stats/page.jsx
@@ -0,0 +1,24 @@
+import Navbar from "@/components/Navbar";
+
+const Stats = () => {
+  return (
+    <div>
+      <Navbar />
+
+      <div className="max-w-5xl mx-auto py-10">
+
+        <h1 className="text-2xl font-bold mb-6">
+          Friendship Analytics
+        </h1>
+
+        <div className="p-6 shadow rounded">
+          Pie Chart Coming Soon
+        </div>
+
+      </div>
+
+    </div>
+  );
+};
+
+export default Stats;
