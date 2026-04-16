eff99e93da02dd6056dd90f3735e76d793ef435b
waliulnizu <waliul.nizu@gmail.com>
2026-04-17 01:52:48 +0600

    added responsive grid layout

--- src/components/Friends.jsx
+++ src/components/Friends.jsx
@@ -3,7 +3,7 @@ import friends from "@/data/friends.json";
 
 const Friends = () => {
   return (
-    <div className="grid grid-cols-4 gap-4 max-w-6xl mx-auto mt-8">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 max-w-6xl mx-auto mt-8">
 
       {
         friends.map(friend => (
