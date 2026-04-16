f7747b7a255a051a3a79777e46f58b233b37d838
waliulnizu <waliul.nizu@gmail.com>
2026-04-17 03:52:56 +0600

    added 404 page

---
+++ src/app/not-found.jsx
@@ -0,0 +1,17 @@
+const NotFound = () => {
+  return (
+    <div className="text-center py-20">
+
+      <h1 className="text-4xl font-bold">
+        404
+      </h1>
+
+      <p className="mt-4">
+        Page Not Found
+      </p>
+
+    </div>
+  );
+};
+
+export default NotFound;
